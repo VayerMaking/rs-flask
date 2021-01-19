fd787ba4b90fb731cd68c6bc22a15630bd9ba5c4
VayerMaking <martin.vayer@gmail.com>
2021-01-19 11:59:39 +0200

    working basic db connections and working post requests

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask import render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+#from active_alchemy import ActiveAlchemy
 import time
 from datetime import datetime
+import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SECRET_KEY'] = config.secret_key
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -17,20 +19,25 @@ class User(db.Model):
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
-      data = request.form
-      user = Note(username=data['username'], password=data['password'])
-      db.session.add(user)
-      db.session.commit()
-      return redirect(url_for('register'))
-      return render_template('register.html')
+        data = request.form
+        user = User(username=data['username'], password=data['password'])
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('register'))
+        return render_template('register.html')
+    elif request.method == "GET":
+        #user = User.query.first()
+        return render_template("register.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
-      data = request.form
-      user = Note(username=data['username'], password=data['password'])
-      return redirect(url_for('login'))
-      return render_template('login.html')
+        data = request.form
+        user = User(username=data['username'], password=data['password'])
+        return redirect(url_for('login'))
+        return render_template('login.html')
+    elif request.method == "GET":
+        return render_template("login.html")
 
 
 if __name__ == '__main__':
