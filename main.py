15236476b1b2122123c3d1e19613564e88bd6c29
Vic-Dim <victor.dimitrov2003@gmail.com>
2021-01-26 11:05:03 +0200

    modified:   main.py 	modified:   templates/index.html 	deleted:    templates/create_post.html 	templates/create_topic.html

--- main.py
+++ main.py
@@ -22,23 +22,32 @@ class User(db.Model):
 def hash_password(password):
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    author = db.Column(db.String)
+    title = db.Column(db.String(30), nullable = False)
+    content = db.Column(db.String, nullable = False)
+    posts = []
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     author = db.Column(db.String)
-    topic = db.Column(db.String)
-    title = db.Column(db.String(30), nullable = False)
     content = db.Column(db.String, nullable = False)
     picture = db.Column(db.String(48), nullable = True)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
-def index():
-    if 'username' in session:
-        username = session['username']
-        return render_template('index.html', username=username)
-    return "You are not logged in <br><a href = '/login'>" + "click here to log in</a>"
+def topic_return():
+	return set(Topic.query.all())
 
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
+def index():
+
+	if request.method == "GET":
+	    if 'username' in session:
+	        username = session['username']
+	        
+	        return render_template('index.html', username=username, topics=topic_return())
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -47,10 +56,10 @@ def register():
         user = User(username=data['username'], password=hash_password(data['password']))
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('register'))
-        return render_template('register.html')
+
+        return render_template('login.html')
     elif request.method == "GET":
-        #user = User.query.first()
+
         return render_template("register.html")
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -59,11 +68,12 @@ def login():
         data = request.form
         user = User.query.filter_by(username=data['username'], password=hash_password(data['password'])).first()
         if not user:
-            return "asdf"
+            return render_template("index.html")
 
         session['username'] = data['username']
 
         return redirect(('/'))
+        
     elif request.method == "GET":
         return render_template("login.html")
 
@@ -72,28 +82,25 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
-@app.route('/post', methods = ['GET', 'POST'])
-def new_post():
+@app.route('/new_topic', methods = ['GET', 'POST'])
+def new_topic():
 
 	if request.method == "POST": 
 		data = request.form
 		
-		post = Post(
+		topic = Topic(
 			   author = session['username']	,	
-			   topic = data['topic'],
 			   title = data['title'],
 			   content = data['content'],
-			   picture = data['picture'],		
 			)
 		
-		db.session.add(post)
+		db.session.add(topic)
 		db.session.commit()
 		
-		return render_template("index.html")
+		return redirect('/')
 	
 	else:
-	
-		return render_template("create_post.html")
+		return render_template("create_topic.html")
 
 
 if __name__ == '__main__':
