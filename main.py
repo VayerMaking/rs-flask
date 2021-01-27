68c7cb2f804183b4f5f60026bc65eb1b29a46711
petardmnv <petar.dmnv@gmail.com>
2021-01-27 21:05:15 +0200

    Add pagination

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ class Post(db.Model):
     topic = db.Column(db.String)
 
 def topic_return():
-	return Topic.query.order_by(Topic.timestamp.asc()).all()
+	page = request.args.get('page', 1, type=int)
+	return Topic.query.order_by(Topic.timestamp.asc()).paginate(page=page, per_page=3)
 
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
