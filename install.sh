ce87dbf2cae0414ea2db5f42a1ff967f4060b4cc
VayerMaking <martin.vayer@gmail.com>
2021-01-28 23:50:09 +0200

    fixed installer bug

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ python3 -m venv rs-flask
 source rs-flask/bin/activate
 pip install -r requirements.txt
 mkdir /static/uploads
-echo "secret_key = "qwerty123"" > config.py
+echo 'secret_key = "qwerty123"' > config.py
